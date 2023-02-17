0f10d18161ea4d127e7b994b00d7008537ee8e70
Julia N'diamoi <ndiamoij@gmail.com>
2023-02-17 15:14:56 +0000

    forgot to commit after each successful change oops

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const path = require("path");
+const port = 8080;
 const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
+const { json } = require("express");
 const todoFilePath = process.env.BASE_JSON_PATH;
 
 // Read todos from todos.json into variable
@@ -18,19 +20,109 @@ app.use(bodyParser.json());
 app.use("/content", express.static(path.join(__dirname, "public")));
 
 app.get("/", (_, res) => {
-  /*
   res.sendFile("./public/index.html", { root: __dirname });
-  */
-  res.status(501).end();
 });
 
 app.get("/todos", (_, res) => {
-  /*
-  res.header("Content-Type","application/json");
+  res.header("Content-Type", "application/json");
   res.sendFile(todoFilePath, { root: __dirname });
-  */
-  res.status(501).end();
 });
+
+app.get("/todos/completed", (_, res) => {
+  res.header("Content-Type", "application/json");
+
+  const profile = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "models/todos.json"))
+  );
+
+  const completedItems = profile.filter((el) => el.completed == true);
+
+  res.send(completedItems);
+});
+
+app.get("/todos/overdue", (_, res) => {
+  res.header("Content-Type", "application/json");
+
+  const currentDate = Date.now();
+
+  const profile = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "models/todos.json"))
+  );
+
+  const overdueItems = profile.filter((el) => new Date(el.due) < currentDate);
+
+  res.send(overdueItems);
+});
+
+app.get("/todos/:id", (_, res) => {
+  res.header("Content-Type", "application/json");
+
+  const profile = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "models/todos.json"))
+  );
+
+  const id = _.params.id;
+
+  const item = profile.find((el) => el.id === id);
+
+  res.send(item);
+});
+
+app.post("/todos", (_, res) => {
+
+  const profile = JSON.parse(fs.readFileSync(path.join(__dirname, "models/todos.json")));
+
+  const body = _.body;
+
+  profile.splice(0, 0, body);
+  
+  fs.writeFile((path.join(__dirname, "models/todos.json")), JSON.stringify(profile), (err) => {
+
+    if (err) {
+
+      const message = "Unable to update. Couldn't write to file";
+      res.status(500).send(message);
+
+    } else {
+
+      const message = "Profile updated";
+      res.status(200).send(message);
+    }
+
+  });
+
+});
+
+app.patch("/todos/:id", (req, res) =>{
+
+  
+  const profile = JSON.parse(fs.readFileSync(path.join(__dirname, "models/todos.json")));
+  
+  const id = req.params.id;
+
+  const item = profile.find((el) => el.id === id);
+
+  item.due = req.body.due;
+  item.name = req.body.name;
+
+  fs.writeFile((path.join(__dirname, "models/todos.json")), JSON.stringify(profile), (err) => {
+
+    if (err) {
+
+      const message = "Unable to update. Couldn't write to file";
+      res.status(500).send(message);
+
+    } else {
+
+      const message = "Profile updated";
+      res.status(200).send(message);
+    }
+
+  });
+
+})
+
+
 
 //Add GET request with path '/todos/overdue'
 
